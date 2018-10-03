2d1c123503ad7feb01e939c94332381117496450
IS Choi <insil321@hotmail.com>
2018-10-03 03:19:29 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,7 +12,7 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-end 
+
 
   #your code here
 contacts.each do |person, data|
@@ -21,6 +21,6 @@ contacts.each do |person, data|
       value.delete_if |value| 
   #remember to return your newly altered contacts hash!
 puts contacts
-
+end
 
 
