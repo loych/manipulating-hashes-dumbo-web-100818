061b1af4f3af1673c0af470e64e0f54056e0bb0f
IS Choi <insil321@hotmail.com>
2018-10-03 03:04:07 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-contacts["Freddy Mercury"][:favorite_icecream_flavors] => "cookie dough", "mint chip"
+contacts["Freddy Mercury"][:favorite_icecream_flavors] => ["cookie dough", "mint chip"]
 end
   #remember to return your newly altered contacts hash!
  puts contacts
